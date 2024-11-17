bbfa485560b2e364f127d54df44f5d9126cb14b2
T1 <kirillkravkchuk@gmail.com>
2024-11-17 12:12:40 +0300

    Parse all the 10 days

--- weatherParser/src/main/java/org/example/Main.java
+++ weatherParser/src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,17 +22,24 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException{
-        DataBaseConnection db = new DataBaseConnection();
-        db.DBConnect("WeatherDB","postgres");
-        Main.StartParse(url1);
+        //DataBaseConnection db = new DataBaseConnection();
+        //db.DBConnect("WeatherDB","postgres");
+        Document page = Parser.getPage(url1);
+        Main.StartParse(page);
     }
 
-    private static void StartParse(URL url) throws IOException {
-        String date = Parse("div[class=date]", url).text();
-        String maxTempString = Parse("div[class=maxt]", url).toString();
-        String maxTemp = ResHandler.handler("[+-]?\\d+|[−]\\d+", maxTempString);
-        String minTempString = Parse("div[class=mint]", url).toString();
-        String minTemp = ResHandler.handler("[+-]?\\d+|[−]\\d+", minTempString);
-        System.out.println("Сегодня, " + date + ", Температура воздуха составит от " + minTemp + " до " + maxTemp + " градудсов");
+    private static void StartParse(Document page) throws IOException {
+        Elements dates = Parse("div[class=date]", page);
+        Element table = Parse("div[class=values]",page).first();
+        Elements maxTempString = Parse("div[class=maxt]", table);
+        //String maxTemp = ResHandler.handler("[+-]?\\d+|[−]\\d+", maxTempString);
+        Elements minTempString = Parse("div[class=mint]", table);
+        //String minTemp = ResHandler.handler("[+-]?\\d+|[−]\\d+", minTempString);
+        for(Element date : dates){
+            System.out.println(date.text());
+            System.out.println(ResHandler.handler("[+-]?\\d+|[−]\\d+", maxTempString.get(dates.indexOf(date)).toString()));
+            System.out.println(ResHandler.handler("[+-]?\\d+|[−]\\d+", minTempString.get(dates.indexOf(date)).toString()));
+        }
+
     }
 }
--- weatherParser/src/main/java/org/example/Parser.java
+++ weatherParser/src/main/java/org/example/Parser.java
@@ -3,16 +3,22 @@ package org.example;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.net.URL;
 
 public class Parser {
-    private static Document getPage(URL urlPar) throws IOException {
+    public static Document getPage(URL urlPar) throws IOException {
         return Jsoup.parse(urlPar,2000);
     }
 
-    public static Element Parse(String el, URL urlPar) throws IOException{
-        Document page = Parser.getPage(urlPar);
-        return page.select(el).first();
+    public static Elements Parse(String el, Document page){
+        Elements item = page.select("div[class=widget-items js-scroll-item]");
+        return item.select(el);
+    }
+
+    public static Elements Parse(String el, Element table){
+        return table.select(el);
     }
 }
